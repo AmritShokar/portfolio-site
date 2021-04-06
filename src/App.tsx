2e0b39c96b0f6d5a4ba7419261473d74db96d42b
Amrit Shokar <ashokar@tveon.com>
2021-04-05 19:32:18 -0700

    Added summary component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import './App.css'
 
 import Landing from './components/landing/Landing'
+import Summary from './components/summary/Summary'
 
 function App() {
   return (
     <div className="App">
       <Landing />
+      <Summary />
     </div>
   );
 }
---
+++ src/components/summary/Summary.tsx
@@ -0,0 +1,11 @@
+
+
+const Summary = () => {
+    return (
+        <div>
+            Hi
+        </div>
+    )
+}
+
+export default Summary
