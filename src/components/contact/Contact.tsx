3885a2203053c77d8bcffc8160d95c23e9f496eb
Amrit Shokar <ashokar@tveon.com>
2021-04-07 16:58:28 -0700

    Added new font for contact info

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,21 +1,32 @@
 import styles from './Contact.module.css'
-import { Box, Grid, Typography } from '@material-ui/core'
+
+import { Box, Grid, Typography, makeStyles, createStyles, Theme } from '@material-ui/core'
 
 import letter from '../../res/letter.png'
 import contact from '../../res/contact.png'
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+   typography: {
+    fontFamily: 'Poppins',
+    fontSize: '2em'
+   }
+  }),
+);
+
 const Contact = () => {
+    const classes = useStyles()
     return (
         <Box className={styles["section"]}>
             <Grid container xs={12} alignItems="center">
                 <Grid item xs={2} />
-                <Grid item xs={5}>
+                <Grid item xs={4}>
                     <img alt="icongeek26" src={letter} className={styles['image']} />
                 </Grid>
                 <Grid item xs={4}>
                     <img alt="contact" src={contact} className={styles['contactImage']} />
                 </Grid>
-                <Grid item xs={1} />
+                <Grid item xs={2} />
             </Grid>
         </Box>
     )
