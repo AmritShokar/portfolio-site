8b202b945a7cd6c5c24eb5516121bcfece91c9a9
Amrit Shokar <ashokar@tveon.com>
2021-04-08 09:07:56 -0700

    Made contact section more responsive

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -4,39 +4,45 @@ import { Box, Grid, Typography, makeStyles, createStyles, Theme, Avatar } from '
 
 import letter from '../../res/letter.png'
 import contact from '../../res/contact.png'
+import { lightGreen } from '@material-ui/core/colors';
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-   typography: { // Delete
-    fontFamily: 'Poppins',
-    fontSize: '2em'
-   },
-   iconAvatar: {
-       height: '5em',
-       width: '6em'
-   },
-   contactAvatar: {
-       height: '5em',
-       width: '13em'
-   }
-  }),
+    createStyles({
+        root: {
+            flexGrow: 1,
+            backgroundColor: '#90EE90',
+            padding: '5em'
+        },
+        typography: { // Delete
+            fontFamily: 'Poppins',
+            fontSize: '2em'
+        },
+        iconAvatar: {
+            height: '5em',
+            width: '6em'
+        },
+        contactAvatar: {
+            height: '5em',
+            width: '13em'
+        }
+    }),
 );
 
 const Contact = () => {
     const classes = useStyles()
     return (
-        <Box className={styles["section"]}>
-            <Grid container xs={12} alignItems="center" justify="center">
-                <Grid item xs={3} />
-                <Grid item xs={2}>
+        <Box className={classes.root}>
+            <Grid container alignItems="center" justify="center" style={{ flexGrow: 1 }}>
+                <Grid item xs={12} sm={2} md={4}/>
+                <Grid item xs={12} sm={4} md={2}>
                     {/* <img alt="icongeek26" src={letter} className={styles['image']} /> */}
                     <Avatar variant="square" src={letter} className={classes.iconAvatar} />
                 </Grid>
-                <Grid item xs={3}>
+                <Grid item xs={12} sm={4} md={2}>
                     {/* <img alt="contact" src={contact} className={styles['contactImage']} /> */}
                     <Avatar variant="square" src={contact} className={classes.contactAvatar} />
                 </Grid>
-                <Grid item xs={2} />
+                <Grid item xs={12} sm={2} md={4}/>
             </Grid>
         </Box>
     )
